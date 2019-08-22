730109358ee7ba845501fa51d72409c45b6d15fc
Victor Aremu <victor.olorunbunmi@gmail.com>
2019-08-22 05:18:37 +0100

    clean up

--- main.js
+++ main.js
@@ -121,6 +121,4 @@ if (!gotTheLock) {
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
 
-
-
 }
