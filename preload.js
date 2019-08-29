78cc113049bd1d4e22059131833fdcd8ec1fdfe7
Victor Aremu <victor.olorunbunmi@gmail.com>
2019-08-29 12:54:21 +0100

    fix path

--- preload.js
+++ preload.js
@@ -15,7 +15,7 @@ const config = new Store({
    projectName: 'SwitchDock'
 });
 
-let trayIcon = new Tray(path.join(__dirname, '/assets/images/switch.png'));
+let trayIcon = new Tray(path.join(__dirname, '/assets/app-icons/icon.png'));
 let settingsWindowOpened = false;
 
 
