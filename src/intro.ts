2e469942c64dc68abe10614afdfd51b16f89dd74
ahkohd <victor.olorunbunmi@gmail.com>
2019-10-23 23:40:42 +0100

    Implement mac specific view change for Intro and Settings page

--- src/intro.ts
+++ src/intro.ts
@@ -1,7 +1,7 @@
 const remote = require('electron').remote;
 const Store = require('electron-store');
 const config = new Store({
-   projectName: 'SwitchDock'
+    projectName: 'SwitchDock'
 });
 
 // Initialize analytics...
@@ -13,88 +13,73 @@ firstUseAnalytics(uuid);
 export class Intro {
     currentIndex: number = 1;
 
-    constructor()
-    {
+    constructor() {
+        if (process.platform == 'darwin') this.macOSCleanUp();
         // add mac specific styling
-        if(remote.process.platform == 'darwin') document.getElementById('slide-mac').classList.add('mac');
+        if (remote.process.platform == 'darwin') document.getElementById('slide-mac').classList.add('mac');
 
         let pane = this.getAllPanes();
         this.showPane(this.currentIndex);
     }
 
-    getAllPanes(): HTMLCollectionOf<HTMLDivElement>
-    {
+    getAllPanes(): HTMLCollectionOf<HTMLDivElement> {
         return document.getElementsByClassName('tip') as HTMLCollectionOf<HTMLDivElement>;
     }
 
-    showPane(paneIndex: number)
-    {
+    showPane(paneIndex: number) {
         let panes = this.getAllPanes();
-        if(paneIndex < 1 || panes == null || panes.length == 0 || paneIndex > panes.length) return;
+        if (paneIndex < 1 || panes == null || panes.length == 0 || paneIndex > panes.length) return;
         this.currentIndex = paneIndex;
-        for(let i = 0; i < panes.length; i++)
-        {
-            if(paneIndex-1 == i)
-            {
+        for (let i = 0; i < panes.length; i++) {
+            if (paneIndex - 1 == i) {
                 panes[i].style.display = 'block';
             } else {
                 panes[i].style.display = 'none';
             }
         }
 
-        if(paneIndex == 1)
-        {
+        if (paneIndex == 1) {
             this.hideElement('prev');
             this.showElement('next');
             this.hideElement('close');
-        } else if(paneIndex == panes.length)
-        {
+        } else if (paneIndex == panes.length) {
             this.hideElement('next');
             this.showElement('prev');
             this.showElement('close');
-        } else
-        {
+        } else {
             this.showElement('prev');
             this.showElement('next');
             this.hideElement('close');
         }
-        
+
     }
 
-    next()
-    {
-        this.showPane(this.currentIndex+1);
+    next() {
+        this.showPane(this.currentIndex + 1);
     }
 
-    prev()
-    {
-        this.showPane(this.currentIndex-1);
+    prev() {
+        this.showPane(this.currentIndex - 1);
     }
 
 
-    hideElement(id: string)
-    {
+    hideElement(id: string) {
         document.getElementById(id).style.display = 'none';
     }
 
-    showElement(id: string)
-    {
+    showElement(id: string) {
         document.getElementById(id).style.display = 'inline';
     }
 
-    close()
-    {
+    close() {
         config.set('showIntro', false);
         remote.getCurrentWindow().close();
     }
-}
 
-// Disable key-combo refresh..
-document.onkeydown = (e) => {
-    const press = (window as any).event ? (window as any).event : e;
-    if (press.keyCode == 82 && press.ctrlKey) {
-        e.preventDefault();
-        e.stopPropagation();
+    macOSCleanUp() {
+        let $macHides = document.getElementsByClassName('if-mac-combo');
+        for (let i = 0; i < $macHides.length; i++) {
+            ($macHides.item(i) as HTMLElement).innerText = '⌘+⌥';
+        }
     }
-
 }
